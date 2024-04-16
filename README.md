726d7ada7e72957f0d761191c54dd22dabc3ce43
SelenSonmez <selensonmez2001@gmail.com>
2024-04-16 22:07:48 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Getir_Bootcamp_Final_Case
---
+++ app/src/main/java/com/getir/finalcase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.getir.finalcase
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.v("selen","selen")
+    }
+}
