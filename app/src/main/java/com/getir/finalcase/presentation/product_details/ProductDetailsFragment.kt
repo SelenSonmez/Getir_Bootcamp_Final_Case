18b518e124051ebbaca9852cbc5dceff720a424c
SelenSonmez <selensonmez2001@gmail.com>
2024-04-21 13:55:32 +0300

    added product list fragment

---
+++ app/src/main/java/com/getir/finalcase/presentation/product_details/ProductDetailsFragment.kt
@@ -0,0 +1,74 @@
+package com.getir.finalcase.presentation.product_details
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.getir.finalcase.R
+import com.getir.finalcase.databinding.FragmentProductDetailsBinding
+import com.getir.finalcase.domain.model.Product
+import com.getir.finalcase.presentation.product_list.ProductListFragmentDirections
+
+
+class ProductDetailsFragment : Fragment() {
+
+    private var binding: FragmentProductDetailsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout and initialize the binding
+        val fragmentBinding = FragmentProductDetailsBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    private fun navigateToProductList() {
+        val action = ProductDetailsFragmentDirections.actionProductDetailsFragmentToProductListFragment()
+        findNavController().navigate(action)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Retrieve the arguments passed from the navigation action
+        val args: ProductDetailsFragmentArgs by navArgs()
+        val product: Product = args.product
+
+
+        // Populate the UI with the product details
+        binding?.apply {
+            textProductName.text = product.name
+            textViewPrice.text = product.priceText
+            textProductAttribute.text = product.attribute
+
+            toolbar.toolbarTitle.text = getString(R.string.title_product_details)
+            toolbar.backButton.visibility = VISIBLE
+
+            toolbar.backButton.setOnClickListener {
+                navigateToProductList()
+            }
+
+            // Load the product image using Glide
+            Glide.with(requireContext())
+                .load(product.imageURL)
+                .placeholder(R.drawable.default_product_image)
+                .into(productImage)
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Release the binding to avoid memory leaks
+        binding = null
+    }
+}
