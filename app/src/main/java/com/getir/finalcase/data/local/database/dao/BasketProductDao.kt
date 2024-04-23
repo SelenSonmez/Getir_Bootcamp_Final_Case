d1f6cf5f6b73def8a6ed0a3fca62fabd011d9a84
SelenSonmez <selensonmez2001@gmail.com>
2024-04-24 02:26:14 +0300

    refactored room queries

--- app/src/main/java/com/getir/finalcase/data/local/database/dao/BasketProductDao.kt
+++ app/src/main/java/com/getir/finalcase/data/local/database/dao/BasketProductDao.kt
@@ -4,9 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.getir.finalcase.data.local.database.entity.BasketProductEntity
 import com.getir.finalcase.data.local.database.entity.ProductEntity
-import com.getir.finalcase.domain.model.BasketProduct
 import kotlinx.coroutines.flow.Flow
 @Dao
 interface BasketProductDao {
@@ -15,23 +13,26 @@ interface BasketProductDao {
    suspend fun insertProduct(productEntity: ProductEntity): Long
 
    @Insert(onConflict = OnConflictStrategy.REPLACE)
-   suspend fun insertBasketProduct(basketProductEntity: BasketProductEntity): Long
-   @Query("SELECT * FROM basket_products")
-    fun getAllBasketProducts(): Flow<List<BasketProductEntity>>
+   suspend fun insertBasketProduct(basketProductEntity: ProductEntity): Long
+   @Query("SELECT * FROM products")
+    fun getAllBasketProducts(): Flow<List<ProductEntity>>
 
-    @Query("SELECT * FROM basket_products WHERE productId = :productId")
-    suspend fun getBasketProductById(productId: String): BasketProductEntity?
+    @Query("SELECT * FROM products WHERE id = :productId")
+    suspend fun getProductById(productId: String): ProductEntity?
 
-    @Query("DELETE FROM basket_products WHERE productId = :productId")
+    @Query("DELETE FROM products WHERE id = :productId")
     suspend fun deleteBasketProductById(productId: String)
 
-    @Query("DELETE FROM basket_products")
+    @Query("DELETE FROM products")
     suspend fun deleteAllBasketProducts()
 
-    @Query("UPDATE basket_products SET count = count - :decrementAmount WHERE productId = :productId")
+    @Query("SELECT * FROM products WHERE id = :productId")
+    suspend fun getProductFromId(productId: String): ProductEntity
+
+    @Query("UPDATE products SET amount = amount - :decrementAmount WHERE id = :productId")
     suspend fun decreaseProductCount(productId: String, decrementAmount: Int = 1)
 
-    @Query("UPDATE basket_products SET count = count + :incrementAmount WHERE productId = :productId")
+    @Query("UPDATE products SET amount = amount + :incrementAmount WHERE id = :productId")
     suspend fun increaseProductCount(productId: String, incrementAmount: Int = 1)
 
 }
